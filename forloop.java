01aad28c1210aeca22986b9a90e1956f9cc76723
Priyanshu Sinha <priyanshusinha0101@gmail.com>
2025-03-26 21:28:02 +0530

    first commit

---
+++ forloop.java
@@ -0,0 +1,16 @@
+package Sample;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		for (x = 1; x <= 10; x++) {
+			System.out.println(x);
+		}
+		for (int y = 1; y <= 10; y++) {
+			System.out.println(y);
+		}
+	}
+
+}
